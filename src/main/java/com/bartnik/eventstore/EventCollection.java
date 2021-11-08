36bc28c9929e38a8d18f238239eb1b34c16b905f
Lukasz A. Bartnik <l.bartnik@gmail.com>
2021-11-07 19:01:26 -0800

    Remove old code

--- src/main/java/com/bartnik/eventstore/EventCollection.java
+++
@@ -1,8 +0,0 @@
-package com.bartnik.eventstore;
-
-public interface EventCollection extends Iterable<SequencedEvent> {
-
-    SequencedEvent first();
-
-    SequencedEvent [] toArray();
-}
--- src/main/java/com/bartnik/eventstore/exception/SerializationError.java
+++
@@ -1,7 +0,0 @@
-package com.bartnik.eventstore.exception;
-
-public class SerializationError extends EventStoreError {
-    public SerializationError(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-}
